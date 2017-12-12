8b9559a5db8ce0826ead1c3575bcf8ba1dfbb5cf
jamcha <jamcha.aa@gmail.com>
2017-12-12 18:27:29 +0900

    Mod manjaro_setup.sh

--- manjaro_setup.sh
+++ manjaro_setup.sh
@@ -4,4 +4,5 @@
 sed -i -e "s/\\#TMPDIR=\"\\/tmp/TMPDIR=\"\\/home\\/user\\/tmp\\/yaourt/g" /etc/yaourtrc
 sed -i -e "s/\\#BUILDDIR=\\/tmp\\/makepkg/BUILDDIR=\\/home\\/user\\/tmp\\/makepkg/g" /etc/makepkg.conf
 mkdir -p /home/user/tmp/yaourt && mkdir /home/user/tmp/makepkg
+chown -R user:user /home/user/tmp
 chown -R user:user /home/user/tmp/*

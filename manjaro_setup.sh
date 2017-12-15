7443a9ded634ea3b8256d176f8374fddeedb53fe
jamcha <jamcha.aa@gmail.com>
2017-12-15 11:56:18 +0900

    Add yaourtrc makepkg.conf backup

--- manjaro_setup.sh
+++ manjaro_setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # reference: https://gae-fan.blogspot.jp/2014/10/yaourt-tmp.html
 
+cp /etc/yaourtrc /etc/yaourtrc.OLD
+cp /etc/makepkg.conf /etc/makepkg.conf.OLD
 sed -i -e "s/\\#TMPDIR=\"\\/tmp/TMPDIR=\"\\/home\\/user\\/tmp\\/yaourt/g" /etc/yaourtrc
 sed -i -e "s/\\#BUILDDIR=\\/tmp\\/makepkg/BUILDDIR=\\/home\\/user\\/tmp\\/makepkg/g" /etc/makepkg.conf
 mkdir -p /home/user/tmp/yaourt && mkdir /home/user/tmp/makepkg

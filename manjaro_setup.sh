2d96d5d2e6a4f8d8d83dec0d8cdd30a2591cea00
jamcha <jamcha.aa@gmail.com>
2017-12-15 12:01:04 +0900

    Add argument

--- manjaro_setup.sh
+++ manjaro_setup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 # reference: https://gae-fan.blogspot.jp/2014/10/yaourt-tmp.html
 
+if [ $# -ne 1 ]; then
+  echo "Insert your name."
+  exit 1
+fi
+
 cp /etc/yaourtrc /etc/yaourtrc.OLD
 cp /etc/makepkg.conf /etc/makepkg.conf.OLD
-sed -i -e "s/\\#TMPDIR=\"\\/tmp/TMPDIR=\"\\/home\\/user\\/tmp\\/yaourt/g" /etc/yaourtrc
-sed -i -e "s/\\#BUILDDIR=\\/tmp\\/makepkg/BUILDDIR=\\/home\\/user\\/tmp\\/makepkg/g" /etc/makepkg.conf
-mkdir -p /home/user/tmp/yaourt && mkdir /home/user/tmp/makepkg
-chown -R user:user /home/user/tmp
-
+sed -i -e "s/\\#TMPDIR=\"\\/tmp/TMPDIR=\"\\/home\\/$#\\/tmp\\/yaourt/g" /etc/yaourtrc
+sed -i -e "s/\\#BUILDDIR=\\/tmp\\/makepkg/BUILDDIR=\\/home\\/$#\\/tmp\\/makepkg/g" /etc/makepkg.conf
+mkdir -p /home/$#/tmp/yaourt && mkdir /home/$#/tmp/makepkg
+chown -R $#:$# /home/$#/tmp
